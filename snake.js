30ba0f9f93a81a63ba295200a0015c1b097999d4
rho <rho.rhoit@gmail.com>
2016-09-24 09:56:05 +0545

    game completed

--- snake.js
+++ snake.js
@@ -15,6 +15,18 @@ var food = new function() {
     this.generate = function() {
         this.x = Math.floor(Math.random() * cols) * cell
         this.y = Math.floor(Math.random() * rows) * cell
+
+        // don't place food on the snake
+        for (var i = 1; i < snake.len; i++) {
+            if (
+                distance(
+                    snake.tail[i][0], this.x,
+                    snake.tail[i][1], this.y
+                ) > 1
+            ) continue
+            this.generate()
+            break
+        }
     }
 
     this.draw = function() {
@@ -28,45 +40,42 @@ var food = new function() {
 }
 
 
+function distance(x0, x1, y0, y1) {
+    return Math.sqrt(Math.pow(x0-x1, 2) + Math.pow(y0-y1, 2))
+}
+
+
+function die(msg) {
+    console.log("dead:", msg)
+    clearInterval(interval)
+    interval = null
+}
+
+
 function bound(val, lower, upper) {
-    if (lower > val || upper < val) {
-        console.log("dead:", val)
-        clearInterval(interval)
-        interval = null
-    }
+    if (lower > val || upper < val) die("wall")
     return val
 }
 
 
-function Snake(x, y, len, speedx, speedy) {
-    this.x = x
-    this.y = y
-    this.len = len
+function Snake(len, dirx, diry) {
+    this.x = 0
+    this.y = cell
 
-    this.tail = [x, y]
+    this.len  = len
+    this.tail = [] // first var is after image
 
-    this.speedx = speedx
-    this.speedy = speedy
-
-    this.speed = function(x, y) {
-        if (this.speedx == -x || this.speedy == -y) return
-        this.speedx = x
-        this.speedy = y
-    }
-
-    this.eat = function(x, y) {
-        var d = Math.sqrt(
-            Math.pow(this.x - x, 2) +
-            Math.pow(this.y - y, 2)
-        )
-        if (d > 1) return false
-        this.len++
-        return true
+    this.dirx = dirx
+    this.diry = diry
+    this.dir = function(x, y) {
+        if (this.dirx == -x || this.diry == -y) return
+        this.dirx = x
+        this.diry = y
     }
 
     this.update = function () {
-        x = this.x + this.speedx * cell
-        y = this.y + this.speedy * cell
+        x = this.x + this.dirx * cell
+        y = this.y + this.diry * cell
 
         this.x = bound(x, 0, width - cell)
         this.y = bound(y, 0, height - cell)
@@ -77,6 +86,21 @@ function Snake(x, y, len, speedx, speedy) {
         this.tail[this.len] = [ this.x, this.y ]
     }
 
+    this.eat = function(x, y) {
+        if (distance(this.x, x, this.y, y) > 1) return false
+        this.len++
+        this.tail[this.len] = [this.x, this.y]
+        return true
+    }
+
+    this.cannibal = function() {
+        for (var i = 1; i < this.len; i++) {
+            if (this.eat(this.tail[i][0], this.tail[i][1])) {
+                die("cannibal")
+                break
+            }
+        }
+    }
 
     this.draw = function() {
         ctx.fillStyle   = '#abffab'
@@ -86,11 +110,23 @@ function Snake(x, y, len, speedx, speedy) {
             this.x+space, this.y+space,
             cell-space, cell-space
         )
-        ctx.clearRect(
-            this.tail[0][0]+space, this.tail[0][1]+space,
-            cell-space, cell-space
-        )
+
+        try {
+            ctx.clearRect(
+                this.tail[0][0]+space, this.tail[0][1]+space,
+                cell-space, cell-space
+            )
+        }
+        catch(err) {
+            console.log(this.tail)
+            die('program crash')
+        }
     }
+
+    for (var i = 0; i <= len; i++) {
+        this.tail[i] = [ this.x, this.y ]
+    }
+    this.update()
 }
 
 
@@ -102,29 +138,29 @@ function keyBinding(event) {
     }
 
     switch (event.keyCode) {
-        case 37: snake.speed(-1, 0); break //LEFT
-        case 38: snake.speed(0, -1); break //UP
-        case 39: snake.speed(+1, 0); break //RIGHT
-        case 40: snake.speed(0, +1); break //DOWN
-        // default:
+        case 37: snake.dir(-1, 0); break //LEFT
+        case 38: snake.dir(0, -1); break //UP
+        case 39: snake.dir(+1, 0); break //RIGHT
+        case 40: snake.dir(0, +1); break //DOWN
     }
 }
 
 
 function gameLoop() {
+    snake.cannibal()
     if (snake.eat(food.x, food.y)) {
         food.generate()
         food.draw()
     }
-    snake.update()
     snake.draw()
+    snake.update()
 }
 
 
 function newGame() {
+    snake = new Snake(3, 1, 0)
     food.generate()
     food.draw()
-    snake = new Snake(2*cell, 2*cell, 1, 1, 0)
     interval = setInterval(gameLoop, 100)
 }
 
